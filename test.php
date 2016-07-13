ab6f04c3dbdba8118d45b897d072783708870aef
sheff4uk <sheff4uk@gmail.com>
2016-07-13 18:56:17 +0300

    fix

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?
 	$from = "admin@fabrikaprestol.ru";
-	$to = "admin@fabrikaprestol.ru";
+	$to = "sheff4uk@gmail.com";
 	$subject = "Подтверждение регистрации Престол";
 	$text = "Здравствуйте! Вы зарегистрировались в Корпоративной Информационной Системе ПРЕСТОЛ\nВаш логин: \nДля активации учетной записи свяжитесь с администрацией: admin@fabrikaprestol.ru\n\nС уважением,\nАдминистрация КИС Престол";
 
@@ -9,9 +9,9 @@
 	$mail = new PHPMailer(true);
 	$mail->isSMTP();
 	$mail->SMTPAuth = true;
-	$mail->SMTPSecure = "tls";
+	$mail->SMTPSecure = "ssl";
 	$mail->Host = "smtp.yandex.ru";
-	$mail->Port = "25";
+	$mail->Port = "465";
 	$mail->Username = "admin@fabrikaprestol.ru";
 	$mail->Password = "GmvN6*D%";
 	$mail->CharSet = "UTF-8";
