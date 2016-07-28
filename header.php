16f4c76a8002b251d0a950669bd4a07dc4748acd
sheff4uk <sheff4uk@gmail.com>
2016-07-28 10:59:23 +0300

    favicon

--- header.php
+++ header.php
@@ -10,7 +10,9 @@
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title><?=$title?></title>
+	<title><?=$title?></title>
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" type='text/css' href="js/ui/jquery-ui.css">
 	<link rel='stylesheet' type='text/css' href='css/style.css'>
 	<link rel='stylesheet' type='text/css' href='css/font-awesome.min.css'>
