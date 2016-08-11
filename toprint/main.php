61aca1bf7ebf0f581f7e87822c161d12dc30a7e4
sheff4uk <sheff4uk@gmail.com>
2016-08-11 15:42:57 +0300

    В таблице для печати сгруппировал ячейки

--- toprint/main.php
+++ toprint/main.php
@@ -66,31 +66,32 @@
         </thead>
         <tbody>
 	<?
-	$query = "SELECT IFNULL(OD.Code, '') Code
+	$query = "SELECT OD.OD_ID
+					,IFNULL(OD.Code, '') Code
 					,IFNULL(OD.ClientName, '') ClientName
 					,DATE_FORMAT(OD.StartDate, '%d.%m.%Y') StartDate
 					,DATE_FORMAT(OD.EndDate, '%d.%m.%Y') EndDate
 					,DATE_FORMAT(OD.ReadyDate, '%d.%m.%Y') ReadyDate
 					,IF(OD.SH_ID IS NULL, 'Свободные', CONCAT(CT.City, '/', SH.Shop)) AS Shop
 					,OD.OrderNumber
-					,CONCAT(IFNULL(GROUP_CONCAT(DISTINCT CONCAT('<i id=\'',ODD.ODD_ID, '\'></i>', ODD.Amount, ' ', IFNULL(PM.Model, '***'), ' ', IFNULL(CONCAT(ODD.Length, 'х', ODD.Width, IFNULL(CONCAT('/', ODD.PieceAmount, 'x', ODD.PieceSize), '')), ''), ' ', IFNULL(PF.Form, ''), ' ', IFNULL(PME.Mechanism, ''), IF(IFNULL(ODD.Comment, '') = '', '', CONCAT(' (', ODD.Comment, ')')), '<br>') ORDER BY IFNULL(PM.PT_ID, 2) DESC, ODD.ODD_ID SEPARATOR ''), ''), IFNULL(GROUP_CONCAT(DISTINCT CONCAT('<i id=\'',ODB.ODB_ID, '\'></i>', ODB.Amount, ' ', BL.Name, IF(IFNULL(ODB.Comment, '') = '', '', CONCAT(' (', ODB.Comment, ')')), '<br>') ORDER BY ODB.ODB_ID SEPARATOR ''), '')) Zakaz
+					,CONCAT(IFNULL(CONCAT(ODD.Amount, ' ', IFNULL(PM.Model, '***'), ' ', IFNULL(CONCAT(ODD.Length, 'х', ODD.Width, IFNULL(CONCAT('/', ODD.PieceAmount, 'x', ODD.PieceSize), '')), ''), ' ', IFNULL(PF.Form, ''), ' ', IFNULL(PME.Mechanism, ''), IF(IFNULL(ODD.Comment, '') = '', '', CONCAT(' (', ODD.Comment, ')'))), ''), IFNULL(CONCAT(ODB.Amount, ' ', BL.Name, IF(IFNULL(ODB.Comment, '') = '', '', CONCAT(' (', ODB.Comment, ')'))), '')) Zakaz
 					,OD.Color
 					,OD.IsPainting
-					,CONCAT(IFNULL(GROUP_CONCAT(DISTINCT CONCAT('<i id=\'t', ODD.ODD_ID, '\'></i>', IF(IFNULL(PM.PT_ID, 2) = 2, IFNULL(CONCAT(ODD.Material,
+					,CONCAT(IFNULL(CONCAT(IF(IFNULL(PM.PT_ID, 2) = 2, IFNULL(CONCAT(ODD.Material,
 						CASE ODD.IsExist
 							WHEN 0 THEN ' <b>(нет)</b>'
 							WHEN 1 THEN ' <b>(заказано)</b>'
 							WHEN 2 THEN ' <b>(есть)</b>'
 						END
-					), ''), ''), '<br>') ORDER BY IFNULL(PM.PT_ID, 2) DESC, ODD.ODD_ID SEPARATOR ''), ''), IFNULL(GROUP_CONCAT(DISTINCT CONCAT('<i id=\'t', ODB.ODB_ID, '\'></i><br>') ORDER BY ODB.ODB_ID SEPARATOR ''), '')) Plastic
-					,CONCAT(IFNULL(GROUP_CONCAT(DISTINCT CONCAT('<i id=\'t', ODD.ODD_ID, '\'></i>', IF(PM.PT_ID = 1, IFNULL(CONCAT(ODD.Material,
+					), ''), '')), '')) Plastic
+					,CONCAT(IFNULL(CONCAT(IF(PM.PT_ID = 1, IFNULL(CONCAT(ODD.Material,
 						CASE ODD.IsExist
 							WHEN 0 THEN ' <b>(нет)</b>'
 							WHEN 1 THEN ' <b>(заказано)</b>'
 							WHEN 2 THEN ' <b>(есть)</b>'
 						END
-					), ''), ''), '<br>') ORDER BY IFNULL(PM.PT_ID, 2) DESC, ODD.ODD_ID SEPARATOR ''), ''), IFNULL(GROUP_CONCAT(DISTINCT CONCAT('<i id=\'t', ODB.ODB_ID, '\'></i><br>') ORDER BY ODB.ODB_ID SEPARATOR ''), '')) Textile
-					,GROUP_CONCAT(DISTINCT CONCAT('<i id=\'s', ODS_WD.itemID, '\'></i>', IFNULL(ODS_WD.Steps, ''), '<br>') ORDER BY ODS_WD.PT_ID DESC, ODS_WD.itemID SEPARATOR '') Steps
+					), ''), '')), '')) Textile
+					,IFNULL(ODS_WD.Steps, '') Steps
 					,IFNULL(OD.Comment, '') Comment
 			  FROM OrdersData OD
 			  LEFT JOIN Shops SH ON SH.SH_ID = OD.SH_ID
@@ -104,7 +105,7 @@
 			  LEFT JOIN (SELECT ODD.OD_ID
 			  				   ,ODD.ODD_ID itemID
 			  				   ,IFNULL(PM.PT_ID, 2) PT_ID
-			  				   ,GROUP_CONCAT(CONCAT(IF(ODS.IsReady, CONCAT('<b>', ST.Short, '</b>'), ST.Short), '(<i>', IFNULL(IFNULL(WD.ShortName, WD.Name), '---'), '</i>)') ORDER BY ST.Sort SEPARATOR ' | ') Steps
+			  				   ,GROUP_CONCAT(CONCAT(IF(ODS.IsReady, CONCAT('<b>', ST.Short, '</b>'), ST.Short), '(<i>', IFNULL(WD.Name, '---'), '</i>)') ORDER BY ST.Sort SEPARATOR '<br>') Steps
 						FROM OrdersDataDetail ODD
 						LEFT JOIN OrdersDataSteps ODS ON ODS.ODD_ID = ODD.ODD_ID AND ODS.Visible = 1 AND ODS.Old = 0
 						LEFT JOIN ProductModels PM ON PM.PM_ID = ODD.PM_ID
@@ -117,34 +118,62 @@
 							  ,0 PT_ID
 							  ,'' Steps
 						FROM OrdersDataBlank ODB
-						) ODS_WD ON ODS_WD.OD_ID = OD.OD_ID
+						) ODS_WD ON ODS_WD.itemID = ODD.ODD_ID
 			  WHERE OD.OD_ID IN ({$id_list})
-			  GROUP BY OD.OD_ID
-			  ORDER BY OD.OD_ID";
-
+			  GROUP BY ODD.ODD_ID
+			  ORDER BY OD.OD_ID, IFNULL(PM.PT_ID, 2) DESC, ODD.ODD_ID";
 	$res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+
+	// Получаем количество изделий в заказе для группировки ячеек
+	$query = "SELECT IFNULL(SUM(ODD_ODB.Cnt), 0) Cnt, OD.OD_ID
+				FROM OrdersData OD
+				LEFT JOIN (
+					SELECT COUNT(1) Cnt, ODD.OD_ID, GROUP_CONCAT(ODD.ODD_ID) Items
+					FROM `OrdersDataDetail` ODD
+					GROUP BY ODD.OD_ID
+					UNION
+					SELECT COUNT(1) Cnt, ODB.OD_ID, GROUP_CONCAT(ODB.ODB_ID) Items
+					FROM `OrdersDataBlank` ODB
+					GROUP BY ODB.OD_ID
+				) ODD_ODB ON ODD_ODB.OD_ID = OD.OD_ID
+				WHERE OD.OD_ID IN ({$id_list})
+				GROUP BY OD.OD_ID
+				ORDER BY OD.OD_ID";
+	$subres = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+	$odid = 0;
 	while( $row = mysqli_fetch_array($res) )
 	{
+		if( $odid != $row["OD_ID"] ) {
+			$subrow = mysqli_fetch_array($subres);
+			$cnt = $subrow["Cnt"];
+			$odid = $row["OD_ID"];
+			$span = 1;
+			$border = " style='border-top: 3px solid black;'";
+		}
+		else {
+			$span = 0;
+			$border = "";
+		}
 		echo "<tr>";
-		if(isset($_GET["CD"])) echo "<td class='nowrap'>{$row["Code"]}</td>";
-		if(isset($_GET["CN"])) echo "<td>{$row["ClientName"]}</td>";
-		if(isset($_GET["SD"])) echo "<td>{$row["StartDate"]}</td>";
-		if(isset($_GET["ED"])) {
+		if(isset($_GET["CD"]) and $span) echo "<td{$border} rowspan='{$cnt}' class='nowrap'>{$row["Code"]}<br>{$cnt}</td>";
+		if(isset($_GET["CN"]) and $span) echo "<td{$border} rowspan='{$cnt}'>{$row["ClientName"]}</td>";
+		if(isset($_GET["SD"]) and $span) echo "<td{$border} rowspan='{$cnt}'>{$row["StartDate"]}</td>";
+		if(isset($_GET["ED"]) and $span) {
 			if( $archive ) {
-				echo "<td>{$row["ReadyDate"]}</td>";
+				echo "<td{$border} rowspan='{$cnt}'>{$row["ReadyDate"]}</td>";
 			}
 			else {
-				echo "<td>{$row["EndDate"]}</td>";
+				echo "<td{$border} rowspan='{$cnt}'>{$row["EndDate"]}</td>";
 			}
 		}
-		if(isset($_GET["SH"])) echo "<td>{$row["Shop"]}</td>";
-		if(isset($_GET["ON"])) echo "<td>{$row["OrderNumber"]}</td>";
-		if(isset($_GET["Z"])) echo "<td><span class='nowrap'>{$row["Zakaz"]}</span></td>";
-		if(isset($_GET["P"])) echo "<td><span class='nowrap'>{$row["Plastic"]}</span></td>";
-		if(isset($_GET["CR"])) echo "<td>{$row["Color"]}</td>";
-		if(isset($_GET["PR"])) echo "<td><span class='nowrap'>{$row["Steps"]}</span></td>";
-		if(isset($_GET["IP"])) {
-			echo "<td>";
+		if(isset($_GET["SH"]) and $span) echo "<td{$border} rowspan='{$cnt}'>{$row["Shop"]}</td>";
+		if(isset($_GET["ON"]) and $span) echo "<td{$border} rowspan='{$cnt}'>{$row["OrderNumber"]}</td>";
+		if(isset($_GET["Z"])) echo "<td{$border}>{$row["Zakaz"]}</td>";
+		if(isset($_GET["P"])) echo "<td{$border}>{$row["Plastic"]}</td>";
+		if(isset($_GET["CR"]) and $span) echo "<td{$border} rowspan='{$cnt}'>{$row["Color"]}</td>";
+		if(isset($_GET["PR"])) echo "<td{$border}><span class='nowrap'>{$row["Steps"]}</span></td>";
+		if(isset($_GET["IP"]) and $span) {
+			echo "<td{$border} rowspan='{$cnt}'>";
 				switch ($row["IsPainting"]) {
 					case 1:
 						echo "<i class='fa fa-star-o fa-lg'></i>";
@@ -158,8 +187,8 @@
 				}
 			echo "</td>";
 		}
-		if(isset($_GET["T"])) echo "<td><span class='nowrap'>{$row["Textile"]}</span></td>";
-		if(isset($_GET["N"])) echo "<td>{$row["Comment"]}</td>";
+		if(isset($_GET["T"])) echo "<td{$border}>{$row["Textile"]}</td>";
+		if(isset($_GET["N"]) and $span) echo "<td{$border} rowspan='{$cnt}'>{$row["Comment"]}</td>";
 	}
     ?>
         </tbody>
