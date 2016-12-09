75016a5bdb057683e7cc570d3a4337438645acc6
sheff4uk <sheff4uk@gmail.com>
2016-12-09 13:49:24 +0300

    Расширенный фильтр в материалах. Возможность редактировать изделие в материалах.

--- materials.php
+++ materials.php
@@ -18,12 +18,16 @@
 	else {
 		$isexist = 0;
 	}
+
 	if( isset($_GET["prod"]) ) {
 		$product = $_GET["prod"];
 	}
 	else {
 		$product = 1;
 	}
+
+	if( $_GET["ready"] == "on" ) unset( $_GET["ready"] );
+	if( $_GET["WD_ID"] == "" ) unset( $_GET["WD_ID"] );
 
 	$MT_IDs = implode(",", $_GET["MT_ID"]);
 	$MT_IDs = $MT_IDs == "" ? "0" : $MT_IDs;
@@ -75,6 +79,8 @@
 		exit ('<meta http-equiv="refresh" content="0; url='.$_SERVER['REQUEST_URI'].'">');
 		die;
 	}
+
+	include "forms.php";
 ?>
 	
 	<form method='get' id='MTfilter'>
@@ -101,18 +107,70 @@
 		</div>
 
 		<div>
+			<label for='ready'>Готовность:&nbsp;</label>
+			<div class='btnset' id='ready'>
+				<input type='radio' id='ready_all' name='ready' checked >
+					<label for='ready_all'>Все</label>
+				<input type='radio' id='ready_0' name='ready' value='0' <?= ($_GET["ready"] == "0" ? "checked" : "") ?>>
+					<label for='ready_0'>Не готово</label>
+				<input type='radio' id='ready_1' name='ready' value='1' <?= ($_GET["ready"] == "1" ? "checked" : "") ?>>
+					<label for='ready_1'>Готово</label>
+			</div>
+		</div>
+
+		<div>
+			<label for='worker'>Работник:&nbsp;</label><br>
+			<select name="WD_ID" id="worker">
+				<option value>Все</option>
+				<option <?= ($_GET["WD_ID"] === "0") ? "selected" : "" ?> value="0">Не назначен</option>
+				<?
+				$query = "SELECT WD.WD_ID, WD.Name
+							FROM WorkersData WD
+							JOIN (
+								SELECT ODS.WD_ID
+								FROM OrdersDataSteps ODS
+								JOIN StepsTariffs ST ON ST.ST_ID = ODS.ST_ID AND ST.Short LIKE '%".( $product == 1 ? "Об" : "Ст" )."%'
+								UNION
+								SELECT ODS.WD_ID
+								FROM OrdersDataSteps ODS
+								JOIN OrdersDataBlank ODB ON ODB.ODB_ID = ODS.ODB_ID
+								JOIN Materials MT ON MT.MT_ID = ODB.MT_ID AND MT.PT_ID = {$product}
+							) ODS ON ODS.WD_ID = WD.WD_ID";
+				$res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+				while( $row = mysqli_fetch_array($res) ) {
+					$selected = ($row["WD_ID"] == $_GET["WD_ID"]) ? "selected" : "";
+					echo "<option {$selected} value='{$row["WD_ID"]}'>{$row["Name"]}</option>";
+				}
+				?>
+			</select>
+		</div>
+
+		<div>
 			<select name="MT_ID[]" multiple style="width: 800px; display: none;">
 				<?
 				$query = "SELECT MT.MT_ID, CONCAT(MT.Material, ' (', IFNULL(SH.Shipper, '-=Другой=-'), ')') Material
 							FROM Materials MT
 							LEFT JOIN Shippers SH ON SH.SH_ID = MT.SH_ID
 							JOIN (
-								SELECT ODD.OD_ID, ODD.MT_ID, ODD.IsExist
+								SELECT ODD.OD_ID, ODD.MT_ID, ODD.IsExist, IFNULL(ODS_ST.WD_ID, 0) WD_ID, IFNULL(ODS_ST.IsReady, 0) IsReady
 								FROM OrdersDataDetail ODD
+								LEFT JOIN (
+									SELECT ODS.ODD_ID, ODS.WD_ID, ODS.IsReady
+									FROM OrdersDataSteps ODS
+									JOIN StepsTariffs ST ON ST.ST_ID = ODS.ST_ID AND (ST.Short LIKE '%Ст%' OR ST.Short LIKE '%Об%')
+									WHERE ODS.Visible = 1 AND ODS.Old != 1 AND ODS.ODD_ID IS NOT NULL
+									GROUP BY ODS.ODD_ID
+								) ODS_ST ON ODS_ST.ODD_ID = ODD.ODD_ID
 								UNION
-								SELECT ODB.OD_ID, ODB.MT_ID, ODB.IsExist
+								SELECT ODB.OD_ID, ODB.MT_ID, ODB.IsExist, IFNULL(ODS_ST.WD_ID, 0) WD_ID, IFNULL(ODS_ST.IsReady, 0) IsReady
 								FROM OrdersDataBlank ODB
-								) ODD_ODB ON ODD_ODB.MT_ID = MT.MT_ID AND ODD_ODB.IsExist = {$isexist}
+								LEFT JOIN (
+									SELECT ODS.ODB_ID, ODS.WD_ID, ODS.IsReady
+									FROM OrdersDataSteps ODS
+									WHERE ODS.Visible = 1 AND ODS.Old != 1 AND ODS.ODB_ID IS NOT NULL
+									GROUP BY ODS.ODB_ID
+								) ODS_ST ON ODS_ST.ODB_ID = ODB.ODB_ID
+							) ODD_ODB ON ODD_ODB.MT_ID = MT.MT_ID AND ODD_ODB.IsExist = {$isexist}".( isset( $_GET["ready"] ) ? " AND ODD_ODB.IsReady = {$_GET["ready"]}" : "" ).( isset( $_GET["WD_ID"] ) ? " AND ODD_ODB.WD_ID = {$_GET["WD_ID"]}" : "" )."
 							LEFT JOIN OrdersData OD ON OD.OD_ID = ODD_ODB.OD_ID
 							WHERE MT.PT_ID = {$product} AND OD.ReadyDate IS NULL
 							GROUP BY MT.MT_ID
@@ -140,6 +198,7 @@
 			<th>Поставщик</th>
 			<th>Код</th>
 			<th>Принят</th>
+			<th>Работник</th>
 			<th>Заказ</th>
 			<th>Цвет</th>
 			<th>Заказчик</th>
@@ -164,23 +223,32 @@
 					,OD.OrderNumber
 					,OD.Comment
 					,OD.IsPainting
-					,OD.confirmed
 					,IFNULL(OD.Color, '<a href=\"/orderdetail.php\">Свободные</a>') Color
 					,IF(DATEDIFF(OD.EndDate, NOW()) <= 7, IF(DATEDIFF(OD.EndDate, NOW()) <= 0, 'bg-red', 'bg-yellow'), '') Deadline
 					,GROUP_CONCAT(ODD_ODB.Zakaz SEPARATOR '') Zakaz
+					,GROUP_CONCAT(ODD_ODB.Zakaz_lock SEPARATOR '') Zakaz_lock
 					,GROUP_CONCAT(ODD_ODB.Material SEPARATOR '') Material
 					,GROUP_CONCAT(ODD_ODB.MN SEPARATOR '') MN
 					,GROUP_CONCAT(ODD_ODB.Shipper SEPARATOR '') Shipper
 					,GROUP_CONCAT(ODD_ODB.Checkbox SEPARATOR '') Checkbox
+					,WD.Name
+					,IF(ODD_ODB.IsReady = 1, 'ready', 'inwork') ready_status
+					,IF(OS.ostatok IS NOT NULL, 1, 0) is_lock
+					,OD.confirmed
 			  FROM OrdersData OD
 			  LEFT JOIN Shops SH ON SH.SH_ID = OD.SH_ID
 			  LEFT JOIN Cities CT ON CT.CT_ID = SH.CT_ID
+			  LEFT JOIN OstatkiShops OS ON OS.year = YEAR(OD.StartDate) AND OS.month = MONTH(OD.StartDate) AND OS.CT_ID = SH.CT_ID
 			  RIGHT JOIN (
 						  SELECT ODD.OD_ID
 								,ODD.ODD_ID ItemID
 								,IFNULL(PM.PT_ID, 2) PT_ID
 
-								,CONCAT('<div', IF(IFNULL(ODD.Comment, '') <> '', CONCAT(' title=\'', ODD.Comment, '\''), ''), '>', IF(IFNULL(ODD.Comment, '') <> '', CONCAT('<i class=\'fa fa-comment\' aria-hidden=\'true\'></i>'), ''), ' ', ODD.Amount, ' ', IFNULL(PM.Model, 'Столешница'), ' ', IFNULL(CONCAT(ODD.Length, IF(ODD.Width > 0, CONCAT('х', ODD.Width), ''), IFNULL(CONCAT('/', IFNULL(ODD.PieceAmount, 1), 'x', ODD.PieceSize), '')), ''), ' ', IFNULL(PF.Form, ''), ' ', IFNULL(PME.Mechanism, ''), '</div>') Zakaz
+							   ,CONCAT('<b style=\'line-height: 1.79em;\'><a ".(in_array('order_add', $Rights) ? "href=\'#\'" : "")." id=\'prod', ODD.ODD_ID, '\' location=\'{$location}\' class=\'".(in_array('order_add', $Rights) ? "edit_product', IFNULL(PM.PT_ID, 2), '" : "")."\'', IF(IFNULL(ODD.Comment, '') <> '', CONCAT(' title=\'', ODD.Comment, '\''), ''), '>', IF(IFNULL(ODD.Comment, '') <> '', CONCAT('<i class=\'fa fa-comment\' aria-hidden=\'true\'></i>'), ''), ' ', ODD.Amount, ' ', IFNULL(PM.Model, 'Столешница'), ' ', IFNULL(CONCAT(ODD.Length, IF(ODD.Width > 0, CONCAT('х', ODD.Width), ''), IFNULL(CONCAT('/', IFNULL(ODD.PieceAmount, 1), 'x', ODD.PieceSize), '')), ''), ' ', IFNULL(PF.Form, ''), ' ', IFNULL(PME.Mechanism, ''), ' ', '</a></b><br>') Zakaz
+
+							   ,CONCAT('<b style=\'line-height: 1.79em;\'><i id=\'prod', ODD.ODD_ID, '\'', IF(IFNULL(ODD.Comment, '') <> '', CONCAT(' title=\'', ODD.Comment, '\''), ''), '>', IF(IFNULL(ODD.Comment, '') <> '', CONCAT('<i class=\'fa fa-comment\' aria-hidden=\'true\'></i>'), ''), ' ', ODD.Amount, ' ', IFNULL(PM.Model, 'Столешница'), ' ', IFNULL(CONCAT(ODD.Length, IF(ODD.Width > 0, CONCAT('х', ODD.Width), ''), IFNULL(CONCAT('/', IFNULL(ODD.PieceAmount, 1), 'x', ODD.PieceSize), '')), ''), ' ', IFNULL(PF.Form, ''), ' ', IFNULL(PME.Mechanism, ''), ' ', '</i></b><br>') Zakaz_lock
+
+							   #,CONCAT('<div', IF(IFNULL(ODD.Comment, '') <> '', CONCAT(' title=\'', ODD.Comment, '\''), ''), '>', IF(IFNULL(ODD.Comment, '') <> '', CONCAT('<i class=\'fa fa-comment\' aria-hidden=\'true\'></i>'), ''), ' ', ODD.Amount, ' ', IFNULL(PM.Model, 'Столешница'), ' ', IFNULL(CONCAT(ODD.Length, IF(ODD.Width > 0, CONCAT('х', ODD.Width), ''), IFNULL(CONCAT('/', IFNULL(ODD.PieceAmount, 1), 'x', ODD.PieceSize), '')), ''), ' ', IFNULL(PF.Form, ''), ' ', IFNULL(PME.Mechanism, ''), '</div>') Zakaz
 
 								,CONCAT('<div class=\'wr_mt\'>', IF(DATEDIFF(ODD.arrival_date, NOW()) <= 0 AND ODD.IsExist = 1, CONCAT('<img src=\'/img/attention.png\' class=\'attention\' title=\'', DATEDIFF(ODD.arrival_date, NOW()), ' дн.\'>'), ''), '<span ptid=\'{$product}\' mtid=\'', ODD.MT_ID, '\' class=\'mt', ODD.MT_ID, IF(MT.removed = 1, ' removed', ''), ' material ".(in_array('screen_materials', $Rights) ? " mt_edit " : "")."',
 								CASE ODD.IsExist
@@ -196,12 +264,22 @@
 
 								,CONCAT('<input type=\'checkbox\' value=\'1\' name=\'prod', ODD.ODD_ID, '\' class=\'chbox\'><br>') Checkbox
 
+								,IFNULL(ODS_ST.WD_ID, 0) WD_ID
+								,IFNULL(ODS_ST.IsReady, 0) IsReady
+
 						  FROM OrdersDataDetail ODD
 						  LEFT JOIN ProductModels PM ON PM.PM_ID = ODD.PM_ID
 						  LEFT JOIN ProductForms PF ON PF.PF_ID = ODD.PF_ID
 						  LEFT JOIN ProductMechanism PME ON PME.PME_ID = ODD.PME_ID
 						  JOIN Materials MT ON MT.MT_ID = ODD.MT_ID
 						  LEFT JOIN Shippers SH ON SH.SH_ID = MT.SH_ID
+						  LEFT JOIN (
+							  SELECT ODS.ODD_ID, ODS.WD_ID, ODS.IsReady
+							  FROM OrdersDataSteps ODS
+							  JOIN StepsTariffs ST ON ST.ST_ID = ODS.ST_ID AND (ST.Short LIKE '%Ст%' OR ST.Short LIKE '%Об%')
+							  WHERE ODS.Visible = 1 AND ODS.Old != 1 AND ODS.ODD_ID IS NOT NULL
+							  GROUP BY ODS.ODD_ID
+						  ) ODS_ST ON ODS_ST.ODD_ID = ODD.ODD_ID
 						  WHERE ODD.IsExist = {$isexist} AND IFNULL(PM.PT_ID, 2) = {$product}
 						  AND (ODD.MT_ID IN ({$MT_IDs}) OR '{$MT_IDs}' = '0')
 						  UNION
@@ -209,7 +287,11 @@
 								,ODB.ODB_ID ItemID
 								,0 PT_ID
 
-								,CONCAT('<div', IF(IFNULL(ODB.Comment, '') <> '', CONCAT(' title=\'', ODB.Comment, '\''), ''), '>', IF(IFNULL(ODB.Comment, '') <> '', CONCAT('<i class=\'fa fa-comment\' aria-hidden=\'true\'></i>'), ''), ' ', ODB.Amount, ' ', IFNULL(BL.Name, ODB.Other), '</div>') Zakaz
+								,CONCAT('<b style=\'line-height: 1.79em;\'><a ".(in_array('order_add', $Rights) ? "href=\'#\'" : "")." id=\'blank', ODB.ODB_ID, '\'', 'class=\'".(in_array('order_add', $Rights) ? "edit_order_blank" : "")."\' location=\'{$location}\'', IF(IFNULL(ODB.Comment, '') <> '', CONCAT(' title=\'', ODB.Comment, '\''), ''), '>', IF(IFNULL(ODB.Comment, '') <> '', CONCAT('<i class=\'fa fa-comment\' aria-hidden=\'true\'></i>'), ''), ' ', ODB.Amount, ' ', IFNULL(BL.Name, ODB.Other), '</a></b><br>') Zakaz
+
+								,CONCAT('<b style=\'line-height: 1.79em;\'><i id=\'blank', ODB.ODB_ID, '\'', IF(IFNULL(ODB.Comment, '') <> '', CONCAT(' title=\'', ODB.Comment, '\''), ''), '>', IF(IFNULL(ODB.Comment, '') <> '', CONCAT('<i class=\'fa fa-comment\' aria-hidden=\'true\'></i>'), ''), ' ', ODB.Amount, ' ', IFNULL(BL.Name, ODB.Other), '</i></b><br>') Zakaz_lock
+
+								#,CONCAT('<div', IF(IFNULL(ODB.Comment, '') <> '', CONCAT(' title=\'', ODB.Comment, '\''), ''), '>', IF(IFNULL(ODB.Comment, '') <> '', CONCAT('<i class=\'fa fa-comment\' aria-hidden=\'true\'></i>'), ''), ' ', ODB.Amount, ' ', IFNULL(BL.Name, ODB.Other), '</div>') Zakaz
 
 								,CONCAT('<div class=\'wr_mt\'>', IF(DATEDIFF(ODB.arrival_date, NOW()) <= 0 AND ODB.IsExist = 1, CONCAT('<img src=\'/img/attention.png\' class=\'attention\' title=\'', DATEDIFF(ODB.arrival_date, NOW()), ' дн.\'>'), ''), '<span ptid=\'{$product}\' mtid=\'', ODB.MT_ID, '\' class=\'mt', ODB.MT_ID, IF(MT.removed = 1, ' removed', ''), ' material ".(in_array('screen_materials', $Rights) ? " mt_edit " : "")."',
 								CASE ODB.IsExist
@@ -225,15 +307,25 @@
 
 								,CONCAT('<input type=\'checkbox\' value=\'1\' name=\'other', ODB.ODB_ID, '\' class=\'chbox\'><br>') Checkbox
 
+								,IFNULL(ODS_ST.WD_ID, 0) WD_ID
+								,IFNULL(ODS_ST.IsReady, 0) IsReady
+
 						  FROM OrdersDataBlank ODB
 						  LEFT JOIN BlankList BL ON BL.BL_ID = ODB.BL_ID
 						  JOIN Materials MT ON MT.MT_ID = ODB.MT_ID
 						  LEFT JOIN Shippers SH ON SH.SH_ID = MT.SH_ID
+						  LEFT JOIN (
+							  SELECT ODS.ODB_ID, ODS.WD_ID, ODS.IsReady
+							  FROM OrdersDataSteps ODS
+							  WHERE ODS.Visible = 1 AND ODS.Old != 1 AND ODS.ODB_ID IS NOT NULL
+							  GROUP BY ODS.ODB_ID
+						  ) ODS_ST ON ODS_ST.ODB_ID = ODB.ODB_ID
 						  WHERE ODB.IsExist = {$isexist} AND MT.PT_ID = {$product}
 						  AND (ODB.MT_ID IN ({$MT_IDs}) OR '{$MT_IDs}' = '0')
 						  ORDER BY PT_ID DESC, ItemID
 						  ) ODD_ODB ON ODD_ODB.OD_ID = OD.OD_ID
-			  WHERE OD.ReadyDate IS NULL
+			  LEFT JOIN WorkersData WD ON WD.WD_ID = ODD_ODB.WD_ID
+			  WHERE OD.ReadyDate IS NULL ".( isset( $_GET["ready"] ) ? "AND ODD_ODB.IsReady = {$_GET["ready"]}" : "" ).( isset( $_GET["WD_ID"] ) ? " AND ODD_ODB.WD_ID = {$_GET["WD_ID"]}" : "" )."
 			  GROUP BY OD.OD_ID
 			  ORDER BY OD.OD_ID";
 	$res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
@@ -255,7 +347,15 @@
 			$title = 'Не принят в работу';
 		}
 		echo "<td class='{$class}' title='{$title}'><i class='fa fa-check-circle fa-2x' aria-hidden='true'></i></td>";
-		echo "<td><span class='nowrap'>{$row["Zakaz"]}</span></td>";
+		echo "<td><span class='{$row["ready_status"]}'>{$row["Name"]}</td>";
+
+		if( $row["is_lock"] or ( $row["confirmed"] and !in_array('order_add_confirm', $Rights) ) ) {
+			echo "<td><span class='nowrap'>{$row["Zakaz_lock"]}</span></td>";
+		}
+		else {
+			echo "<td><span class='nowrap'>{$row["Zakaz"]}</span></td>";
+		}
+
 		switch ($row["IsPainting"]) {
 			case 1:
 				echo "<td class='notready' title='Не в работе'>{$row["Color"]}</td>";
@@ -277,13 +377,66 @@
 		echo "<td>{$row["OrderNumber"]}</td>";
 		echo "<td>{$row["Comment"]}</td>";
 		echo "</tr>";
+
+		// Заполнение массива для JavaScript
+		$query = "SELECT ODD.ODD_ID
+						,ODD.Amount
+						,ODD.Price
+						,ODD.PM_ID
+						,ODD.PF_ID
+						,ODD.PME_ID
+						,ODD.Length
+						,ODD.Width
+						,ODD.PieceAmount
+						,ODD.PieceSize
+						,ODD.Comment
+						,IFNULL(MT.Material, '') Material
+						,IFNULL(MT.SH_ID, '') Shipper
+						,ODD.IsExist
+                        ,DATE_FORMAT(ODD.order_date, '%d.%m.%Y') order_date
+                        ,DATE_FORMAT(ODD.arrival_date, '%d.%m.%Y') arrival_date
+						,IF(SUM(ODS.WD_ID) IS NULL, 0, 1) inprogress
+				  FROM OrdersDataDetail ODD
+				  LEFT JOIN OrdersDataSteps ODS ON ODS.ODD_ID = ODD.ODD_ID AND ODS.Visible = 1
+				  LEFT JOIN Materials MT ON MT.MT_ID = ODD.MT_ID
+				  WHERE ODD.OD_ID ".( $row["OD_ID"] == "" ? "IS NULL" : "= {$row["OD_ID"]}" )."
+				  GROUP BY ODD.ODD_ID";
+		$result = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+		while( $sub_row = mysqli_fetch_array($result) )
+		{
+			$ODD[$sub_row["ODD_ID"]] = array( "amount"=>$sub_row["Amount"], "price"=>$sub_row["Price"], "model"=>$sub_row["PM_ID"], "form"=>$sub_row["PF_ID"], "mechanism"=>$sub_row["PME_ID"], "length"=>$sub_row["Length"], "width"=>$sub_row["Width"], "PieceAmount"=>$sub_row["PieceAmount"], "PieceSize"=>$sub_row["PieceSize"], "color"=>$sub_row["Color"], "comment"=>$sub_row["Comment"], "material"=>$sub_row["Material"], "shipper"=>$sub_row["Shipper"], "isexist"=>$sub_row["IsExist"], "inprogress"=>$sub_row["inprogress"], "order_date"=>$sub_row["order_date"], "arrival_date"=>$sub_row["arrival_date"] );
+		}
+
+		$query = "SELECT ODB.ODB_ID
+						,ODB.Amount
+						,ODB.Price
+						,ODB.BL_ID
+						,ODB.Other
+						,ODB.Comment
+						,IFNULL(MT.Material, '') Material
+						,IFNULL(MT.SH_ID, '') Shipper
+						,ODB.IsExist
+						,IF(SUM(ODS.WD_ID) IS NULL, 0, 1) inprogress
+						,DATE_FORMAT(ODB.order_date, '%d.%m.%Y') order_date
+						,DATE_FORMAT(ODB.arrival_date, '%d.%m.%Y') arrival_date
+						,IFNULL(MT.PT_ID, 0) MPT_ID
+				  FROM OrdersDataBlank ODB
+				  LEFT JOIN OrdersDataSteps ODS ON ODS.ODB_ID = ODB.ODB_ID AND ODS.Visible = 1
+				  LEFT JOIN Materials MT ON MT.MT_ID = ODB.MT_ID
+				  WHERE ODB.OD_ID ".( $row["OD_ID"] == "" ? "IS NULL" : "= {$row["OD_ID"]}" )."
+				  GROUP BY ODB.ODB_ID";
+		$result = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+		while( $sub_row = mysqli_fetch_array($result) )
+		{
+			$ODB[$sub_row["ODB_ID"]] = array( "amount"=>$sub_row["Amount"], "price"=>$sub_row["Price"], "blank"=>$sub_row["BL_ID"], "other"=>$sub_row["Other"], "comment"=>$sub_row["Comment"], "material"=>$sub_row["Material"], "shipper"=>$sub_row["Shipper"], "isexist"=>$sub_row["IsExist"], "inprogress"=>$sub_row["inprogress"], "order_date"=>$sub_row["order_date"], "arrival_date"=>$sub_row["arrival_date"], "MPT_ID"=>$sub_row["MPT_ID"] );
+		}
 	}
 ?>
 		</tbody>
 	</table>
 
 	<!-- Список материалов для буфера обмена -->
-	<textarea id='materials_name' style='position: absolute; top: 32px; left: 1px; height: 20px; z-index: -1;'><?=$materials_name?></textarea>
+	<textarea id='materials_name' style='position: absolute; top: 34px; left: 1px; height: 20px; z-index: -1;'><?=$materials_name?></textarea>
 
 	<p><input type='checkbox' id='selectallbottom'><label for='selectallbottom'>Выбрать все</label></p>
 	<p>
@@ -384,7 +537,11 @@
 			});
 		});
 
-		$('#isexist input, #material input').change(function(){
+		$('#material input').change(function(){
+			$('select[name="WD_ID"] option').removeAttr('selected');
+		});
+
+		$('#isexist input, #material input, #ready input, #worker').change(function(){
 			$('select[name="MT_ID[]"] option').removeAttr('selected');
 			$('#MTfilter').submit();
 		});
@@ -395,5 +552,78 @@
 			closeOnSelect: false,
 			language: "ru"
 		});
+
+		$( ".textiletags" ).autocomplete({ // Автокомплит тканей
+			source: "autocomplete.php?do=textiletags",
+			minLength: 2,
+			select: function( event, ui ) {
+				$('select[name="Shipper"]').val(ui.item.SH_ID);
+			},
+			create: function() {
+				$(this).data('ui-autocomplete')._renderItem = function( ul, item ) {
+					var listItem = $( "<li>" )
+						.append( item.label )
+						.appendTo( ul );
+
+					if (item.removed == 1) {
+						listItem.addClass( "removed" ).attr( "title", "Выведен!" )
+					}
+
+					return listItem;
+				}
+			}
+		});
+
+		$( ".plastictags" ).autocomplete({ // Автокомплит пластиков
+			source: "autocomplete.php?do=plastictags",
+			minLength: 2,
+			select: function( event, ui ) {
+				$('select[name="Shipper"]').val(ui.item.SH_ID);
+			},
+			create: function() {
+				$(this).data('ui-autocomplete')._renderItem = function( ul, item ) {
+					var listItem = $( "<li>" )
+						.append( item.label )
+						.appendTo( ul );
+
+					if (item.removed == 1) {
+						listItem.addClass( "removed" ).attr( "title", "Выведен!" )
+					}
+
+					return listItem;
+				}
+			}
+		});
+
+		$( ".textileplastictags" ).autocomplete({ // Автокомплит материалов
+			source: "autocomplete.php?do=textileplastictags",
+			minLength: 2,
+			select: function( event, ui ) {
+				$('select[name="Shipper"]').val(ui.item.SH_ID);
+			},
+			create: function() {
+				$(this).data('ui-autocomplete')._renderItem = function( ul, item ) {
+					var listItem = $( "<li>" )
+						.append( item.label )
+						.appendTo( ul );
+
+					if (item.removed == 1) {
+						listItem.addClass( "removed" ).attr( "title", "Выведен!" )
+					}
+
+					return listItem;
+				}
+			}
+		});
+
+		// При очистке поля с материалом - очищаем поставщика
+		$( ".textiletags, .plastictags, .textileplastictags" ).on("keyup", function() {
+			if( $(this).val().length < 2 ) {
+				$('select[name="Shipper"]').val('');
+			}
+		});
+
+		odd = <?= json_encode($ODD) ?>;
+		odb = <?= json_encode($ODB) ?>;
 	});
 </script>
