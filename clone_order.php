5696ead73e7f8b4dca46ce424f186ad282a01a3a
sheff4uk <sheff4uk@gmail.com>
2017-07-25 10:49:14 +0300

    При клонировании новый заказ без дат и неопределенным статусом материалов

--- clone_order.php
+++ clone_order.php
@@ -4,20 +4,20 @@
 	if( isset($_GET["id"]) ) {
 		$AddDate = date("Y-m-d");
 		$query = "INSERT INTO OrdersData(CLientName, AddDate, StartDate, EndDate, SH_ID, OrderNumber, Color, Comment, creator, confirmed)
-				  SELECT CLientName, '{$AddDate}', StartDate, EndDate, SH_ID, OrderNumber, Color, Comment, {$_GET["author"]}, {$_GET["confirmed"]}
+				  SELECT CLientName, '{$AddDate}', NULL, NULL, SH_ID, OrderNumber, Color, Comment, {$_GET["author"]}, {$_GET["confirmed"]}
 				  FROM OrdersData
 				  WHERE OD_ID = {$_GET["id"]}";
 		mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 		$id = mysqli_insert_id( $mysqli );
 
 		$query = "INSERT INTO OrdersDataDetail(OD_ID, PM_ID, Length, Width, PieceAmount, PieceSize, PF_ID, PME_ID, MT_ID, IsExist, Amount, Price, Comment, order_date, arrival_date, creator, patina)
-				  SELECT {$id}, PM_ID, Length, Width, PieceAmount, PieceSize, PF_ID, PME_ID, MT_ID, IsExist, Amount, Price, Comment, order_date, arrival_date, {$_GET["author"]}, patina
+				  SELECT {$id}, PM_ID, Length, Width, PieceAmount, PieceSize, PF_ID, PME_ID, MT_ID, NULL, Amount, Price, Comment, order_date, arrival_date, {$_GET["author"]}, patina
 				  FROM OrdersDataDetail
 				  WHERE OD_ID = {$_GET["id"]} AND Del = 0";
 		mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 
 		$query = "INSERT INTO OrdersDataBlank(OD_ID, BL_ID, Other, Amount, Price, Comment, MT_ID, IsExist, order_date, arrival_date, creator, patina)
-				  SELECT {$id}, BL_ID, Other, Amount, Price, Comment, MT_ID, IsExist, order_date, arrival_date, {$_GET["author"]}, patina
+				  SELECT {$id}, BL_ID, Other, Amount, Price, Comment, MT_ID, NULL, order_date, arrival_date, {$_GET["author"]}, patina
 				  FROM OrdersDataBlank
 				  WHERE OD_ID = {$_GET["id"]} AND Del = 0";
 		mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
