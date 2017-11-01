c8ff95357919cba0baa99bc2ca2b98967dbf5569
sheff4uk <sheff4uk@gmail.com>
2017-11-01 12:34:06 +0300

    fix

--- print_forms_list.php
+++ print_forms_list.php
@@ -85,7 +85,7 @@ $query = "SELECT PF_ID
 			LEFT JOIN Users USR ON USR.USR_ID = PF.USR_ID
 			LEFT JOIN Kontragenty KAp ON KAp.KA_ID = PF.platelshik_id
 			LEFT JOIN Kontragenty KAg ON KAg.KA_ID = PF.gruzopoluchatel_id
-			WHERE IFNULL(PF.summa, 0) > 0 AND year = {$year}".(in_array('print_forms_view_autor', $Rights) ? " AND PF.USR_ID = {$_SESSION['id']}" : "").($payer ? " AND KA.KA_ID = {$payer}" : "")."
+			WHERE IFNULL(PF.summa, 0) > 0 AND year = {$year}".(in_array('print_forms_view_autor', $Rights) ? " AND PF.USR_ID = {$_SESSION['id']}" : "").($payer ? " AND (KAp.KA_ID = {$payer} OR KAg.KA_ID = {$payer})" : "")."
 			ORDER BY PF.PF_ID DESC";
 $res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 while( $row = mysqli_fetch_array($res) ) {
