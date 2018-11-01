caa80741b57b137a025b39ca17c99df940ba907c
sheff4uk <sheff4uk@gmail.com>
2018-11-01 11:18:15 +0300

    test stalled

--- header2.php
+++ header2.php
@@ -200,23 +200,23 @@
 //			$('#loading').hide();
 
 			//Проверка браузера
-			$.reject({
-				reject: {
-					safari: true, // Apple Safari
-					//chrome: true, // Google Chrome
-					//firefox: true, // Mozilla Firefox
-					msie: true, // Microsoft Internet Explorer
-					//opera: true, // Opera
-					konqueror: true, // Konqueror (Linux)
-					unknown: true // Everything else
-				},
-				close: false,
-				display: ['chrome','firefox','opera'],
-				header: 'Ваш браузер устарел',
-				paragraph1: 'Вы пользуетесь устаревшим браузером, который не поддерживает современные веб-стандарты и представляет угрозу безопасности Ваших данных.',
-				paragraph2: 'Пожалуйста, установите современный браузер:',
-				closeMessage: ''
-			});
+//			$.reject({
+//				reject: {
+//					safari: true, // Apple Safari
+//					//chrome: true, // Google Chrome
+//					//firefox: true, // Mozilla Firefox
+//					msie: true, // Microsoft Internet Explorer
+//					//opera: true, // Opera
+//					konqueror: true, // Konqueror (Linux)
+//					unknown: true // Everything else
+//				},
+//				close: false,
+//				display: ['chrome','firefox','opera'],
+//				header: 'Ваш браузер устарел',
+//				paragraph1: 'Вы пользуетесь устаревшим браузером, который не поддерживает современные веб-стандарты и представляет угрозу безопасности Ваших данных.',
+//				paragraph2: 'Пожалуйста, установите современный браузер:',
+//				closeMessage: ''
+//			});
 
 			// Принудительное перемещение к якорю после перезагрузки страницы
 			var loc = window.location.hash.replace("#","");
