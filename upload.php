2138fc707338043a1fe4ebd1190584e0fc1deefd
sheff4uk <sheff4uk@gmail.com>
2019-01-22 13:03:18 +0300

    fix uploads

--- upload.php
+++ upload.php
@@ -45,8 +45,8 @@ if (copy($_FILES['uploadfile']['tmp_name'], $uploadfile))
 	$comment = mysqli_real_escape_string($mysqli, $comment);
 //	$comment = convert_str($_POST["comment"]);
 //	$comment = mysqli_real_escape_string($mysqli, $comment);
-//	$query = "INSERT INTO OrdersAttachments SET OD_ID = {$_POST["odid"]}, filename = '{$filename}', comment = '{$comment}'";
-//	mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
+	$query = "INSERT INTO OrdersAttachments SET OD_ID = {$_POST["odid"]}, filename = '{$filename}', comment = '{$comment}'";
+	mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 
 	$_SESSION["success"][] = "Файл ".$_FILES['uploadfile']['name']." успешно загружен на сервер.";
 }
