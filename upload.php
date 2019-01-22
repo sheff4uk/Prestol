453d9dc43a94273f71c6266334c795e9aff799df
sheff4uk <sheff4uk@gmail.com>
2019-01-22 12:35:56 +0300

    test uploads

--- upload.php
+++ upload.php
@@ -37,9 +37,9 @@ $filename = date('U').'_'.$_FILES['uploadfile']['name'];
 $uploaddir = './uploads/';
 $uploadfile = $uploaddir.basename($filename);
 
-//// Копируем файл из каталога для временного хранения файлов:
-//if (copy($_FILES['uploadfile']['tmp_name'], $uploadfile))
-//{
+// Копируем файл из каталога для временного хранения файлов:
+if (copy($_FILES['uploadfile']['tmp_name'], $uploadfile))
+{
 //	// Записываем в БД информацию о файле
 //	$comment = convert_str($_POST["comment"]);
 //	$comment = mysqli_real_escape_string($mysqli, $comment);
@@ -47,10 +47,10 @@ $uploadfile = $uploaddir.basename($filename);
 //	mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 //
 //	$_SESSION["success"][] = "Файл ".$_FILES['uploadfile']['name']." успешно загружен на сервер.";
-//}
-//else {
-//	$_SESSION["alert"][] = "Ошибка! Не удалось загрузить файл на сервер!";
-//}
+}
+else {
+	$_SESSION["alert"][] = "Ошибка! Не удалось загрузить файл на сервер!";
+}
 
 exit ('<meta http-equiv="refresh" content="0; url=/orderdetail.php?id='.$_POST["odid"].'&tabs=2">');
 ?>
