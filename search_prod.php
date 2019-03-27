39baa87cca1ac8a5059f9c7e846f7834349a3668
sheff4uk <sheff4uk@gmail.com>
2019-03-27 12:50:00 +0300

    fix

--- search_prod.php
+++ search_prod.php
@@ -3,7 +3,7 @@
 	include "config.php";
 	include "checkrights.php";
 
-	$datediff = 1000; // Максимальный период отображения данных
+	$datediff = 730; // Максимальный период отображения данных
 
 	$term = convert_str($_GET["term"]);
 	$term = mysqli_real_escape_string($mysqli, $term);
@@ -21,7 +21,7 @@
 		FROM OrdersData OD
 		JOIN OrdersDataDetail ODD ON ODD.OD_ID = OD.OD_ID
 		LEFT JOIN Shops SH ON SH.SH_ID = OD.SH_ID
-		WHERE OD.DelDate IS NULL AND OD.Code LIKE '%{$term}%' AND IFNULL(SH.CT_ID, 0) IN ({$USR_cities}) AND OD.StartDate IS NULL AND ((OD.ReadyDate IS NOT NULL AND DATEDIFF(NOW(), OD.ReadyDate) <= {$datediff}) OR (OD.ReadyDate IS NULL))
+		WHERE OD.DelDate IS NULL AND OD.Code LIKE '%{$term}%' AND IFNULL(SH.CT_ID, 0) IN ({$USR_cities}) AND ((OD.ReadyDate IS NOT NULL AND DATEDIFF(NOW(), OD.ReadyDate) <= {$datediff}) OR (OD.ReadyDate IS NULL))
 	";
 
 	$res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
