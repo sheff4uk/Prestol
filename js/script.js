4ffe3e412ea3fb6aa573e14efd602916fe13c754
sheff4uk <sheff4uk@gmail.com>
2019-04-18 01:01:44 +0300

    Настоящая защита от двойного сабмита

--- js/script.js
+++ js/script.js
@@ -85,9 +85,10 @@ function makeform(odd_id, location)
 
 ////////////////////////////////////////////////////////////////////////////////
 $(function(){
-	// Предотвращение двойного клика
-	$("*").dblclick(function(e){
-		e.preventDefault();
+	// Предотвращение двойного сабмита формы
+	$('form button').click(function() {
+		$('form button').prop("disabled", true);
+		$(this).parents('form').submit();
 	});
 
 	// При очистке информации о цвете деактивируем кнопки прозрачности
