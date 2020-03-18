19fd20e7ac825a26a818ae59e451dd12cb6b0a50
sheff4uk <sheff4uk@gmail.com>
2020-03-18 19:13:07 +0300

    fix

--- boxing.php
+++ boxing.php
@@ -22,6 +22,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 	<link rel='stylesheet' type='text/css' href='css/style.css'>
