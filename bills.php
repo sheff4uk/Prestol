a906422264d16882176cbf194e6f61862cbe70d6
sheff4uk <sheff4uk@gmail.com>
2020-09-28 15:32:12 +0300

    Все счета от Престол

--- bills.php
+++ bills.php
@@ -249,7 +249,7 @@ if( isset($_GET["add_bill"]) ) {
 	$_POST["nomer"] = $count;
 
 	// Информация о продавце
-	$query = "SELECT * FROM Rekvizity WHERE R_ID = ".($_GET["CT_ID"] == 24 ? "3" : "1");
+	$query = "SELECT * FROM Rekvizity WHERE R_ID = 1";
 	$res = mysqli_query( $mysqli, $query ) or die("Invalid query: " .mysqli_error( $mysqli ));
 	$_POST["destination_name"] = mysqli_result($res,0,'Name');
 	$_POST["destination_adres"] = mysqli_result($res,0,'Addres');
