1ae64820ab16eb8c6bc203244e45e745e8992689
sheff4uk <sheff4uk@gmail.com>
2021-10-14 00:22:13 +0300

    fix

--- api/v2/receipts/index.php
+++ api/v2/receipts/index.php
@@ -1,6 +1,9 @@
 <?
 include "../../../config.php";
 
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
 message_to_telegram("token: ".$_SERVER['Authorization'], '217756119');
-message_to_telegram("id: ".$_GET["id"], '217756119');
+message_to_telegram("body: ".$json, '217756119');
 ?>
