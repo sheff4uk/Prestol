20cab3d6655b4b04064bb5a6a816faf2c3b86608
sheff4uk <sheff4uk@gmail.com>
2021-10-19 15:46:50 +0300

    fix

--- api/v2/receipts/index.php
+++ api/v2/receipts/index.php
@@ -1,9 +1,11 @@
 <?
 include "../../../config.php";
 
-$json = file_get_contents('php://input');
-$data = json_decode($json, true);
+//$json = file_get_contents('php://input');
+//$data = json_decode($json, true);
+//
+//message_to_telegram("token: ".$_SERVER['Authorization'], '217756119');
+//message_to_telegram("body: ".$json, '217756119');
 
-message_to_telegram("token: ".$_SERVER['Authorization'], '217756119');
-message_to_telegram("body: ".$json, '217756119');
+message_to_telegram('test', '217756119');
 ?>
