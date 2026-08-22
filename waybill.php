f88fe7049b0a405acad390de38110cb123d1bb35
sheff4uk <sheff4uk@gmail.com>
2026-08-22 09:50:02 +0300

    Исправлена товарно-транспотрная накладная

--- waybill.php
+++ waybill.php
@@ -99,24 +99,28 @@ $headers = stream_context_create(array(
 	)
 ));
 
-// $path = file_get_contents('https://service-online.su/forms/auto/ttn/blanc.php', false, $headers);
-// $path = strstr($path, '/blank/');
-// $path = strstr($path, '.pdf', true);
-// header('Content-Type: application/pdf');
-// echo file_get_contents('https://service-online.su'.$path.'.pdf', false, null);
-
 $content = file_get_contents('https://service-online.su/forms/auto/ttn/blanc.php', false, $headers);
-// Проверяем, были ли редиректы в заголовках
-if (isset($http_response_header)) {
-	foreach ($http_response_header as $header) {
-		if (strpos(strtolower($header), 'location:') !== false) {
-			// Получаем последний Location
-			$path = trim(substr($header, 9));
-			$path = strstr($path, '/blank/');
-			$path = strstr($path, '.pdf', true);
-			header('Content-Type: application/pdf');
-			echo file_get_contents('https://service-online.su'.$path.'.pdf', false, null);
+
+	// Извлечение пути к файлу из заголовков
+	if (isset($http_response_header)) {
+		foreach ($http_response_header as $header) {
+			if (strpos(strtolower($header), 'pdfhandoff') !== false) {
+
+				// 1. Извлекаем закодированное значение куки pdfhandoff
+				preg_match('/pdfhandoff=([^;]+)/', $header, $matches);
+					
+				// 2. Декодируем URL-символы (%7B -> {, %22 -> " и т.д.)
+				$json_string = urldecode($matches[1]);
+				
+				// 3. Декодируем полученную JSON-строку в массив
+				$data = json_decode($json_string, true);
+				
+				// 4. Забираем нужный ключ
+				$file_path = $data['file'] ?? null;
+				
+				header('Content-Type: application/pdf');
+				echo file_get_contents('https://service-online.su'.$file_path, false, null);
+			}
 		}
 	}
-}
 ?>
